dc0953a5b8e040ccdb87b2f839182f21a6ed72de
Aalaahammooda <aalaa.hamoudh93@gmail.com>
2024-05-07 16:55:43 +0300

    coverback

--- script.js
+++ script.js
@@ -66,6 +66,9 @@ let cards = document.querySelectorAll(".box")
 let firstCard = null
 let secondCard = null
 
+let firstCover = null
+let secondCover = null
+
 let cardContainer = null
 
 const startGame = () => {
@@ -96,14 +99,22 @@ const flipCard = (event) => {
   if (firstCard) {
     // Second card has been selected
     secondCard = cardContainer.querySelector(".firstface")
+    secondCover = cardContainer.querySelector(".cover")
+
     compareCards()
   } else {
     // First card has been selected
     firstCard = cardContainer.querySelector(".firstface")
+    firstCover = cardContainer.querySelector(".cover")
   }
 }
 
 const compareCards = () => {
+  
+  //const firstCover=firstCover.querySelector(".cover")
+  //const secoundCover=secoundCover.querySelector(".cover")
+
+
   const firstCardColor = firstCard.getAttribute("data-value")
   const secondCardColor = secondCard.getAttribute("data-value")
 
@@ -119,14 +130,14 @@ const compareCards = () => {
     // Flip cards back to cover
     firstCard.classList.toggle("show")
     // Step 1 hide second card
+    secondCard.classList.toggle("show")
     // Step 2 show first card cover
+    firstCover.classList.toggle("hide")
     // Step 3 show second card cover
-
+    secondCover.classList.toggle("show")
     
+  
   }
 }
- 
-
- 
 
 startGame()

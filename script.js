22e24d50ac7865c29278214c61c1e393f5371eea
Aalaahammooda <aalaa.hamoudh93@gmail.com>
2024-05-08 14:52:59 +0300

    restart

--- script.js
+++ script.js
@@ -9,11 +9,10 @@
 // Call on a function that will put random images in the array
 
 let cards = document.querySelectorAll(".box")
+const restart = document.querySelector("#restart")
 //let cards = []
 
- //let cardBoard = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
-
- 
+//let cardBoard = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
 
 let firstCard = null
 let secondCard = null
@@ -30,23 +29,21 @@ const startGame = () => {
 }
 //to choose card random
 
-
-
 //const shuffleCards=() =>{
-  // console.log(`originalCards `, originalCards)
-  // // While originalCards has cards inside of it
-  // while(originalCards.length > 0){
-  //   // Generate random number between 0 and the length of originalCards
-  //   const randomIndex = Math.floor(Math.random() * (0, originalCards.length)) 
-  //   console.log(`randomIndex => ${randomIndex}`)
-  //   // Get the card at random location generated above and put in cards
-  //   const randomCard = originalCards[randomIndex]
-  //   // TODO add card to cards
-  //   cards.push(randomCard)
-  //   // Remove random card that was selected from original cards
-  //   originalCards.splice(randomIndex, 1)
-  // }
-  // console.log(`cards `, cards)
+// console.log(`originalCards `, originalCards)
+// // While originalCards has cards inside of it
+// while(originalCards.length > 0){
+//   // Generate random number between 0 and the length of originalCards
+//   const randomIndex = Math.floor(Math.random() * (0, originalCards.length))
+//   console.log(`randomIndex => ${randomIndex}`)
+//   // Get the card at random location generated above and put in cards
+//   const randomCard = originalCards[randomIndex]
+//   // TODO add card to cards
+//   cards.push(randomCard)
+//   // Remove random card that was selected from original cards
+//   originalCards.splice(randomIndex, 1)
+// }
+// console.log(`cards `, cards)
 //}
 
 // function letRandom(min ,max){
@@ -55,7 +52,6 @@ const startGame = () => {
 // }
 // r=letRandom(0,8)
 // console.log(letRandom)
-
 
 const flipEventListeners = () => {
   cards.forEach((card) => {
@@ -70,8 +66,6 @@ const flipCard = (event) => {
   // Display block for .firstFace & Display none for .cover
   cardContainer.querySelector(".firstface").classList.toggle("show")
   cardContainer.querySelector(".cover").classList.toggle("hide")
-  
-
 
   // Is this the first or second card
   if (firstCard) {
@@ -88,18 +82,14 @@ const flipCard = (event) => {
 }
 
 const compareCards = () => {
-  
   //const firstCover=firstCover.querySelector(".cover")
   //const secoundCover=secoundCover.querySelector(".cover")
-
 
   const firstCardColor = firstCard.getAttribute("data-value")
   const secondCardColor = secondCard.getAttribute("data-value")
 
   // Compare here
-  
-    
-  
+
   if (firstCardColor === secondCardColor) {
     console.log("It's a match")
     firstCard.parentElement.removeEventListener("click", flipCard)
@@ -110,32 +100,39 @@ const compareCards = () => {
     console.log("Not a match")
 
     setTimeout(() => {
-    // Flip cards back to cover
-    firstCard.classList.toggle("show")
-    // Step 1 hide second card
-    secondCard.classList.toggle("show")
-    // Step 2 show first card cover
-    firstCover.classList.toggle("hide")
-    // Step 3 show second card cover
-    secondCover.classList.toggle("hide")
-    firstCard=null
-    secondCard=null
-    },500)
+      // Flip cards back to cover
+      firstCard.classList.toggle("show")
+      // Step 1 hide second card
+      secondCard.classList.toggle("show")
+      // Step 2 show first card cover
+      firstCover.classList.toggle("hide")
+      // Step 3 show second card cover
+      secondCover.classList.toggle("hide")
+      firstCard = null
+      secondCard = null
+    }, 500)
   }
 }
+function restartGame() {
+  let firstCard = null
+  let secondCard = null
+  
+  let firstCover = null
+  let secondCover = null
+  let cardContainer = null
+  cards.forEach((card) => {
+    card.querySelector(".firstface").classList.remove("show");
+    card.querySelector(".cover").classList.remove("hide");
 
 
+    cards.forEach((card) => {
+      card.removeEventListener("click", flipCard);
+    });
+  });
 
-
-
-
-
-
-
+  startGame()
+}
 
 startGame()
-
-
-
-
-
+const againGame = document.getElementById("restart")
+restart.addEventListener("click", restartGame)
